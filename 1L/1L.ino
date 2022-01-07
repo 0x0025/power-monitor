6947db120b07e3d61ef21b6e5e9f66d9ff55e670
0x25 <40762411+0x0025@users.noreply.github.com>
2022-01-07 20:30:57 +0300

    1L работает

--- 1L/1L.ino
+++ 1L/1L.ino
@@ -1,36 +1,76 @@
-#include <LiquidCrystal_I2C.h>
+//#define USE_V3
 
-LiquidCrystal_I2C lcd(0x27,16,2);  // set the LCD address to 0x27 for a 20 chars and 4 line display
+#define PZEM_RX_PIN 12
+#define PZEM_TX_PIN 11
+
+
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+#include <SoftwareSerial.h>
+
+#ifdef USE_V3
+#include <PZEM004Tv30.h>
+PZEM004Tv30 pzem(PZEM_RX_PIN, PZEM_TX_PIN);
+#endif
+
+#ifndef USE_V3
+#include <PZEM004T.h>         //Использовать старую версию библиотеки
+IPAddress ip(192,168,1,1);
+PZEM004T pzem(PZEM_RX_PIN,PZEM_TX_PIN);
+#endif
+
+LiquidCrystal_I2C lcd(0x27,16,2);
 
 float v, a, w, wh;
-
 bool power;
 
 void setup(){
-  v = 230.1;
-  a = 0.8;
-  w = 127.5;
-  wh = 94.3;
   
+  Serial.begin(115200);
   lcd.begin();
- 
   lcd.backlight();
+
+  #ifndef USE_V3
+  pzem.setAddress(ip);
+  #endif
 }
 
 void loop(){
+  #ifdef USE_V3
+  v = pzem.voltage();
+  a = pzem.current();
+  w = pzem.power();
+  wh = pzem.energy();
+  #endif
+
+  #ifndef USE_V3
+  v = pzem.voltage(ip);
+  a = pzem.current(ip);
+  w = pzem.power(ip);
+  wh = pzem.energy(ip);
+  #endif
+  
+  Serial.println(String(v));
   updDisplay();
   
 }
 
 void updDisplay(){
-  lcd.setCursor(0, 0);
-  lcd.print(formatCell(v) + "V " + formatCell(a) + "A");
-  
-  lcd.setCursor(0, 1);
-  lcd.print(formatCell(w) + "W " + formatCell(wh) + "Wh");
+  if ( (v <= 0) || (isnan(v)) ){
+    lcd.setCursor(0, 0);
+    lcd.print("       NO       ");
+    lcd.setCursor(0, 1);
+    lcd.print("      POWER     ");
+  } else {
+    lcd.setCursor(0, 0);
+    lcd.print(formatVal(v) + "V " + formatVal(a) + "A");
+    
+    lcd.setCursor(0, 1);
+    lcd.print(formatVal(w) + "W " + formatVal(wh) + "Wh");
+  }
 }
 
-String formatCell(float val) {
+String formatVal(float val) {
   if (isnan(val)){
     return "   0.0";
   } 
